224a23c2c032138eeea3a2f89de18c7bc4d2d0e7
Welch <weq511762338@gmail.com>
2025-01-11 23:22:07 -0500

    added amount

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = () => {
                 <TabsContent value="manual">
                     <div className='w-full '>
                         <Label htmlFor="amount" style={{ color: 'rgb(177, 186, 211)', fontFamily: 'plinko_m', fontWeight: 'bold' }}>Amount</Label>
-                        <div className='relative'>
+                        <div className='relative mr-16'>
                             <Input
                                 id="amount"
                                 type='number'
@@ -29,20 +29,33 @@ const Sidebar = () => {
                                 onChange={(e) => {
                                     const value = parseFloat(e.target.value);
                                     setAmount(isNaN(value) ? 0.00 : value);
-                                    console.log(amount);
+                                    // console.log(amount);
                                 }}
                                 onBlur={(e) => {
                                     const value = parseFloat(e.target.value);
                                     setAmount(isNaN(value) ? 0.00 : parseFloat(value.toFixed(2)));
                                     e.target.value = value.toFixed(2);
                                 }}
-                                className='pr-8'
+                                className='pr-8 rounded-none'
                             />
                             <svg fill="none" viewBox="0 0 96 96" className="svg-icon absolute right-3 top-3" style={{ width: '16px', height: '16px' }}>
                                 <title></title>
                                 <path d="M48 96c26.51 0 48-21.49 48-48S74.51 0 48 0 0 21.49 0 48s21.49 48 48 48Z" fill="#FFC800"></path>
                                 <path d="M48.16 21.92c10.16 0 16.56 4.92 20.32 10.72l-8.68 4.72c-2.28-3.44-6.48-6.16-11.64-6.16-8.88 0-15.36 6.84-15.36 16.12 0 9.28 6.48 16.12 15.36 16.12 4.48 0 8.44-1.84 10.6-3.76v-5.96H45.68v-8.96h23.4v18.76c-5 5.6-12 9.28-20.88 9.28-14.32 0-26.12-10-26.12-25.44C22.08 31.92 33.84 22 48.2 22l-.04-.08Z" fill="#473800"></path>
                             </svg>
+                            <button
+                                className="h-10 w-10 absolute top-0 -right-10 rounded-none transition duration-300 transform bg-[rgb(47,69,83)] hover:bg-[rgb(80,110,130)] flex items-center justify-center"
+                                onClick={() => setAmount((prevAmount) => parseFloat((prevAmount / 2).toFixed(2)))}
+                            >
+                                ½
+                            </button>
+                            <button
+                                className="h-10 w-10 absolute top-0 -right-20 rounded-tr rounded-br transition duration-300 transform bg-[rgb(47,69,83)] hover:bg-[rgb(80,110,130)] text-sm flex items-center justify-center"
+                                onClick={() => setAmount((prevAmount) => parseFloat((prevAmount * 2).toFixed(2)))}
+                            >
+                                2×
+                            </button>
+                            <div className="h-6 w-0.5 bg-[rgb(26,44,56)] absolute top-1/2 transform -translate-y-1/2 -right-10 rounded"></div>
                         </div>
                     </div>
                     <div className='w-full'>
